4f14ae1c9e5f0c92ea4f7876cb64883ae05facf4
Nestor Pylat <nestor.pylat@justanswer.com>
2021-11-19 17:36:39 +0200

    React Calculator

---
+++ my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import InputComponent from "./components/InputComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <InputComponent />
+    </div>
+  );
+}
+
+export default App;
